7ac76395ad1bd7075115d9c925e37e95d73fa8b1
Anıl <Anl@ANL>
2025-02-07 14:45:53 +0300

    Add project files.

---
+++ AjaxNightNew/Context/AjaxContext.cs
@@ -0,0 +1,15 @@
+﻿using AjaxNightNew.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AjaxNightNew.Context
+{
+    public class AjaxContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=Anıl\\SQLEXPRESS;initial Catalog=AjaxNightDb;integrated Security=true");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
